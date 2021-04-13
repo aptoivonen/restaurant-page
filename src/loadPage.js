c4e20ef5b14f8098d96a7a1caef91ecefd7c5773
Aleksi Toivonen <aptoivonen@mail.com>
2021-04-13 10:47:35 +0300

    Add call to action button

--- src/loadPage.js
+++ src/loadPage.js
@@ -1,29 +1,39 @@
 function init(doc) {
   const root = doc.getElementById("content");
+  makeHeader();
+  makeMain();
 
-  const header = doc.createElement("header");
-  root.appendChild(header);
+  function makeHeader() {
+    const header = doc.createElement("header");
+    root.appendChild(header);
 
-  const h1 = doc.createElement("h1");
-  h1.textContent = "My Sea Food Restaurant";
-  header.appendChild(h1);
+    const h1 = doc.createElement("h1");
+    h1.textContent = "My Sea Food Restaurant";
+    header.appendChild(h1);
 
-  const container = doc.createElement("div");
-  container.className = "container";
-  header.appendChild(container);
+    const container = doc.createElement("div");
+    container.className = "container";
+    header.appendChild(container);
 
-  const panel = doc.createElement("div");
-  panel.className = "panel";
-  container.appendChild(panel);
+    const panel = doc.createElement("div");
+    panel.className = "panel";
+    container.appendChild(panel);
 
-  const firstPara = doc.createElement("p");
-  firstPara.textContent =
-    "My restaurant offers a wide variaty of sea food for every mouth.";
-  panel.appendChild(firstPara);
+    const firstPara = doc.createElement("p");
+    firstPara.textContent =
+      "My restaurant offers a wide variaty of sea food for every mouth.";
+    panel.appendChild(firstPara);
 
-  const secondPara = doc.createElement("p");
-  secondPara.textContent = "Grab your bite!";
-  panel.appendChild(secondPara);
+    const callToAction = doc.createElement("a");
+    callToAction.href = "#main";
+    callToAction.className = "call-to-action";
+    callToAction.textContent = "Grab your bite!";
+    panel.appendChild(callToAction);
+  }
+
+  function makeMain() {
+    const main = doc.createElement("main");
+  }
 }
 
 export { init };
