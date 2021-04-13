57d8e617a90edb3f72e0c7d51eb0c57a92610175
Aleksi Toivonen <aptoivonen@mail.com>
2021-04-13 10:47:05 +0300

    Make tab switching logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import { init } from "./loadPage";
 
 init(document);
+
+const tabsButtonsGroup = document.querySelector(".tabs-buttons");
+const activeTabButtonClass = "tab-button-active";
+const tabsSections = document.querySelectorAll(".tab-section");
+const activeTabSectionClass = "tab-section-active";
+
+function toggleTab(e) {
+  if (!e.target.classList.contains("tab-button")) return;
+  removeActiveFromButtonsAndTabs();
+  const index = parseInt(e.target.dataset.index, 10);
+  makeButtonAndTabActive(index);
+}
+
+function removeActiveFromButtonsAndTabs() {
+  tabsButtonsGroup
+    .querySelectorAll(".tab-button")
+    .forEach((button) => button.classList.remove(activeTabButtonClass));
+  tabsSections.forEach((section) =>
+    section.classList.remove(activeTabSectionClass)
+  );
+}
+
+function makeButtonAndTabActive(index) {
+  tabsButtonsGroup
+    .querySelectorAll(".tab-button")
+    [index].classList.add(activeTabButtonClass);
+  tabsSections[index].classList.add(activeTabSectionClass);
+}
+
+tabsButtonsGroup.addEventListener("click", toggleTab);
