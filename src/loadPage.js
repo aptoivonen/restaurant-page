938628d663831098c840c27949d9a713dae24c5e
Aleksi Toivonen <aptoivonen@mail.com>
2021-04-13 12:34:35 +0300

    Refactor loadPage

--- src/loadPage.js
+++ src/loadPage.js
@@ -1,39 +1,10 @@
-function init(doc) {
-  const root = doc.getElementById("content");
-  makeHeader();
-  makeMain();
+import createHeader from "./header";
+import createMain from "./main";
 
-  function makeHeader() {
-    const header = doc.createElement("header");
-    root.appendChild(header);
-
-    const h1 = doc.createElement("h1");
-    h1.textContent = "My Sea Food Restaurant";
-    header.appendChild(h1);
-
-    const container = doc.createElement("div");
-    container.className = "container";
-    header.appendChild(container);
-
-    const panel = doc.createElement("div");
-    panel.className = "panel";
-    container.appendChild(panel);
-
-    const firstPara = doc.createElement("p");
-    firstPara.textContent =
-      "My restaurant offers a wide variaty of sea food for every mouth.";
-    panel.appendChild(firstPara);
-
-    const callToAction = doc.createElement("a");
-    callToAction.href = "#main";
-    callToAction.className = "call-to-action";
-    callToAction.textContent = "Grab your bite!";
-    panel.appendChild(callToAction);
-  }
-
-  function makeMain() {
-    const main = doc.createElement("main");
-  }
+function init() {
+  const root = document.getElementById("content");
+  root.appendChild(createHeader());
+  root.appendChild(createMain());
 }
 
-export { init };
+export default init;
